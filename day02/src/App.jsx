b0e473df4d51cc7aa7419fa8313e80cce51748db
winshine0326 <shane10340@gmail.com>
2025-03-12 10:55:57 +0900

    children props 사용하기 : children prop에 jsx 전달하기

--- day02/src/App.jsx
+++ day02/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import CompoExtract from './CompoExtract.jsx'
+import ChildProps from './ChildProps.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <CompoExtract/>
+      <ChildProps/>
     </>
   )
 }
---
+++ day02/src/ChildProps.jsx
@@ -0,0 +1,30 @@
+
+export default function ChildProps() {
+  return (
+    <div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={100}
+          height={100}
+        />
+      </Card>
+      <Card title="About">
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
+    </div>
+  );
+}
+
+const Card = ({ children, title })=>{
+    return(
+        <div className="card">
+            <div className="card-content">
+                <h1>{title}</h1>
+                { children }
+            </div>
+        </div>
+    )
+}
