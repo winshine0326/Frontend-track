a2eb8c012a34b78836fc24030f35132346e04146
winshine0326 <shane10340@gmail.com>
2025-03-19 11:13:47 +0900

    게시글 목록 만들기

--- day03/src/App.jsx
+++ day03/src/App.jsx
@@ -4,6 +4,7 @@ import MapTest2 from './MapTest2'
 import MapTest3 from './MapTest3'
 import MapTest4 from './MapTest4'
 import PackingList from './PackingList'
+import PostList from './PostList'
  
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       {/* <MapTest2/> */}
       {/* <PackingList/> */}
       {/* <MapTest3/> */}
-      <MapTest4/>
+      {/* <MapTest4/> */}
+      <PostList/>
     </div>
   )
 }
