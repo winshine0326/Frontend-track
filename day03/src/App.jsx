cb4dd6c6ddeab2bd7322e4fa7dea9f1a7cf1da37
winshine0326 <shane10340@gmail.com>
2025-03-19 10:24:29 +0900

    Map 렌더링 과제

--- day03/src/App.jsx
+++ day03/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import MapTest1 from './MapTest1'
 import MapTest2 from './MapTest2'
+import MapTest3 from './MapTest3'
+import MapTest4 from './MapTest4'
 import PackingList from './PackingList'
  
 function App() {
@@ -8,7 +10,9 @@ function App() {
     <div>
       {/* <MapTest1/> */}
       {/* <MapTest2/> */}
-      <PackingList/>
+      {/* <PackingList/> */}
+      {/* <MapTest3/> */}
+      <MapTest4/>
     </div>
   )
 }
---
+++ day03/src/MapTest4.jsx
@@ -0,0 +1,80 @@
+import './Maptest3.css';
+
+function getImageUrl(imageId, size = 's') {
+    return (
+        'https://i.imgur.com/' +
+        imageId +
+        size +
+        '.jpg'
+    );
+}
+
+const MapTest4 = () => {
+    const people = [
+        {
+            id: 0,
+            name: '크레올라 캐서린 존슨',
+            profession: '수학자',
+            accomplishment: '우주비행 계산',
+            imageId: 'MK3eW3A',
+        },
+        {
+            id: 1,
+            name: '마리오 호세 몰리나-파스켈 헨리케스',
+            profession: '화학자',
+            accomplishment: '북극 오존 홀 발견',
+            imageId: 'mynHUSa',
+        },
+        {
+            id: 2,
+            name: '모하메드 압두스 살람',
+            profession: '물리학자',
+            accomplishment: '전자기학 이론',
+            imageId: 'bE7W1ji',
+        },
+        {
+            id: 3,
+            name: '퍼시 레이본 줄리언',
+            profession: '화학자',
+            accomplishment: '코르티손 약물, 스테로이드 및 피임약 개발',
+            imageId: 'IOjWm71',
+        },
+        {
+            id: 4,
+            name: '수브라흐마니안 찬드라세카르',
+            profession: '천체물리학자',
+            accomplishment: '백색 왜성의 질량 계산',
+            imageId: 'lrWQx8l',
+        },
+    ];
+    const chemists = people.filter((item,index)=>item.profession=="화학자");
+    return (
+        <div>
+            <h1>Scientists</h1>
+            <h1>화학자들</h1>
+            <ul>
+                {chemists.map((item) => {
+                    return(
+                        <li className='card'>
+                            <img src={getImageUrl(item.imageId)}/>
+                            <p><b>{item.name}</b>:{item.profession} known from {item.accomplishment}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+            <h1>기타</h1>
+            <ul>
+                {people.map((item) => {
+                    return(
+                        <li className='card'>
+                            <img src={getImageUrl(item.imageId)}/>
+                            <p><b>{item.name}</b>:{item.profession} known from {item.accomplishment}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
+}
+
+export default MapTest4;
