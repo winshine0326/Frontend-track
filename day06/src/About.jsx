b8073b04d3a3cb6c984edc95fc7e9e58d562986a
winshine0326 <shane10340@gmail.com>
2025-04-09 10:07:04 +0900

    react router

---
+++ day06/src/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = ()=>{
+    return <h1>소개 페이지입니다.</h1>
+}
+
+export default About;
--- day06/src/App.jsx
+++ day06/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
 import React from 'react'
 import UseEffect1 from './UseEffect1'
+import { Link, Routes,Route } from 'react-router-dom'
+import About from './About'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
-      <UseEffect1/>
+    <nav>
+      <Link to="/">홈</Link> | <Link to="/about">소개</Link>
+    </nav>
+      {/* <UseEffect1/> */}
+
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
     </>
   )
 }
