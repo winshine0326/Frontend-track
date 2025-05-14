d6fc282b97f9b526702ca987c1a627cac44c341b
winshine0326 <shane10340@gmail.com>
2025-05-14 11:27:29 +0900

    feature detailtab

--- practice-router/src/App.tsx
+++ practice-router/src/App.tsx
@@ -1,12 +1,10 @@
 //App.tsx
 import "./styles.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Router from './Router';
 import Card from "./components/Card";
 import NoPage from "./components/NoPage.jsx";
 import CardList from "./CardList";
 import Home from "./components/Home";
-import Detail from "./components/Detail.js";
 import DetailTab from "./components/DetailTab.js";
 
 export default function App() {
